47aec6baf03d812692bbf8eb42a23ecdb2779e30
Obed0101 <dibujos.obed@gmail.com>
2024-11-20 21:24:38 -0500

    test

--- assets/js/my.js
+++ assets/js/my.js
@@ -119,4 +119,13 @@ premyfucn();
   });
 
 
+  const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+
 
